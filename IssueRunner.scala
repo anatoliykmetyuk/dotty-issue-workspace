74932b1f84ebdfef6b816df8d5f78e0249880cb1
Anatolii <anatoliykmetyuk@gmail.com>
2019-10-03 12:50:25 +0200

    Ditch awk

--- IssueRunner.scala
+++ IssueRunner.scala
@@ -1,49 +1,51 @@
 import sbt._, sbt.internal.util.AttributeMap
 import Keys._
 import complete.DefaultParsers._
+
+import java.io.File
+
 import scala.sys.process._
+import scala.io.Source
+
 
 object IssueRunner extends AutoPlugin {
   override def requires = sbt.plugins.JvmPlugin
   override def trigger = allRequirements
 
-  val issuesWorkspaceAttr = AttributeKey[String]("issueWorkspace")
+  val issuesWorkspaceAttr = AttributeKey[File]("issueWorkspace")
 
   def issuesWorkspace = Command.args("issuesWorkspace", "<workdir>") { case (state, dirName :: Nil) =>
-    val newState = state.copy(attributes = state.attributes.put(issuesWorkspaceAttr, dirName))
+    val newState = state.copy(attributes = state.attributes.put(issuesWorkspaceAttr, new File(dirName)))
     println(s"Issues Workspace is set to ${newState.attributes(issuesWorkspaceAttr)}")
     newState
   }
 
-  def issue = Command.args("issue", "<dirName>") { case (state, dirName :: Nil) =>
-    @annotation.tailrec def locateLaunchFile(dirRaw: String): String = {
-      val dir = dirRaw.reverse.dropWhile(_ == '/').reverse
-      val launchFile = s"$dir/launch.iss"
-      println(s"Attempting to load $launchFile")
-      if (file(launchFile).exists) launchFile
-      else if (augmentString(dir).filter(_ != '/').isEmpty) throw new RuntimeException("Can't locate the launch file")
-      else locateLaunchFile(dir.reverse.dropWhile(_ != '/').reverse)
-    }
+  @annotation.tailrec def locateLaunchFile(dir: File): File = {
+    if (dir eq null) throw new RuntimeException("Can't locate the launch file")
 
+    val launchFile = new File(dir, "launch.iss")
+    println(s"Attempting to load $launchFile")
+    if (launchFile.exists) launchFile
+    else locateLaunchFile(dir.getParentFile)
+  }
+
+  def issue = Command.args("issue", "<dirName>") { case (state, dirName :: Nil) =>
     val issuesWorkspace = state.attributes(issuesWorkspaceAttr)
-    val issueDir  = s"$issuesWorkspace/$dirName"
+    val issueDir  = new File(issuesWorkspace, dirName)
     val launchSrc = locateLaunchFile(issueDir)
 
-    val compileLaunchCmd = List(
-      "awk",
-      "-F", "#",
-      """
-      |/^[[:space:]]*#/ || /^$/ { next }
-      |!/^[[:space:]]/ { printf ";%s",$1; next }
-      |{ printf "%s",$1 }
-      |""".stripMargin,
-      launchSrc)
+    val launchCmd = Source.fromFile(launchSrc).getLines
+      .map { line =>
+        val commented = line.takeWhile(_ != '#')
+        if (commented.headOption.filter(c => !c.isWhitespace).isDefined) s";$commented"
+        else commented
+      }
+      .filter(line => !line.isEmpty && !line.forall(_.isWhitespace))
+      .mkString(" ")
+      .replace("$here", issueDir.getPath)
 
-    val res = compileLaunchCmd.lineStream.head
-      .replace("$here", issueDir)
-
-    println(s"Executing command:\n$res")
-    Command.process(res, state)
+    println(s"Executing command:\n$launchCmd")
+    Command.process(launchCmd, state)
   }
 
   override lazy val projectSettings = Seq(commands ++= Seq(issue, issuesWorkspace))
